dd591e2d1a2452c62f73c5f00978ff0e67b2b6e8
Daniel Carapia <toonlink4943@gmail.com>
2017-06-23 10:51:00 -0600

    Removed old project to place the portfolio site I've worked on

--- amountChange.js
+++
@@ -1,8 +0,0 @@
-var count = 0;
-function incrementIngredientAmount(){
-    document.getElementById("ingredientQuantity").innerHTML = count++;
-}
-
-function decrementIngredientAmount(){
-    document.getElementById("ingredientQuantity").innerHTML = count--;
-}
--- imageUpload.js
+++
@@ -1,4 +0,0 @@
-function PreviewImage(event) {
-    var output = document.getElementById('PreviewImageID');
-    output.src = URL.createObjectURL(event.target.files[0]);
-}
